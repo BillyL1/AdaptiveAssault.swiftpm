2aad1c312dcd55e4fd873f6d49ac535e05c3d446
Billy <wlinker4256@stu.d214.org>
2024-04-19 14:42:36 -0500

    added a health variable

--- Enemy.swift
+++ Enemy.swift
@@ -11,9 +11,11 @@ import SwiftUI
 struct EnemyView: View {
     @State var xpos = 0.0
     @State var ypos = 0.0
+    @State var health = 10
     var body: some View {
         Rectangle()
             .position(x: xpos, y: ypos)
+            
         
     }
 }
