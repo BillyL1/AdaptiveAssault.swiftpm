0d1238d5ea86c492906783a9dc35c279b0c333d2
Austin Witt <awitt4481@stu.d214.org>
2024-05-03 14:54:07 -0500

    gave texts in towersview backgrounds

--- TowersView.swift
+++ TowersView.swift
@@ -11,23 +11,63 @@ import SwiftUI
 struct TowersView:View {
     var body: some View {
         HStack{
-            VStack{
-                Text("Musketeer")
-                Text("POW!.....POW!")
-                Text("Fueled with the hatred of learning, this musketeer will do everything she can to halt these shapes in their path")
-
+            ZStack{
+                RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                    .stroke(Color.gray, lineWidth: 7)
+                    .background(
+                        RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                            .fill(Color.brown))
+                    .frame(width: 310, height: 190, alignment: .center)
+                VStack{
+                    Text("Musketeer")
+                        .font(.system(size:40))
+                        .bold()
+                    Text("POW!.....POW!")
+                        .font(.system(size:30))
+                        .bold()
+                    Text("Fueled with the hatred of learning, this musketeer will do everything she can to halt these shapes in their path")
+                        .bold()
+                }
+                .frame(width: 300, height: 300)
             }
-            VStack{
-                Text("Wizard")
-                Text("Magic is knowlage")
-                Text("Education is what ruined the wizard's reputation, so why not go after where education begins, the shapes")
-
+            ZStack{
+                RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                    .stroke(Color.gray, lineWidth: 7)
+                    .background(
+                        RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                            .fill(Color.brown))
+                    .frame(width: 310, height: 190, alignment: .center)
+                VStack{
+                    Text("Wizard")
+                        .font(.system(size:40))
+                        .bold()
+                    Text("Magic is knowlage")
+                        .font(.system(size:20))
+                        .bold()
+                    Text("Education is what ruined the wizard's reputation, so why not go after where education begins, the shapes")
+                        .bold()
+                }
+                .frame(width: 300, height: 300)
+                    
             }
-            VStack{
-                Text("Tesla")
-                Text("ZAP")
-                Text("Made by the wizard to do his biddings, this tower fights with precision and speed")
-
+            ZStack{
+                RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                    .stroke(Color.gray, lineWidth: 7)
+                    .background(
+                        RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                            .fill(Color.brown))
+                    .frame(width: 310, height: 190, alignment: .center)
+                VStack{
+                    Text("Tesla")
+                        .font(.system(size:40))
+                        .bold()
+                    Text("ZAP")
+                        .font(.system(size:30))
+                        .bold()
+                    Text("Made by the wizard to do his biddings, this tower fights with precision and speed")
+                        .bold()
+                }
+                .frame(width: 300, height: 300)
             }
 
         }
